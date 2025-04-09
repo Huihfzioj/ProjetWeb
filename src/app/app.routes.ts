8423eb70f2a1cd10888813eb3dde65e7e58df80b
Huihfzioj <hediljlassi64@gmail.com>
2025-04-09 12:02:28 +0100

    routing prob

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TermsComponent } from './terms/terms.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  { path: '', component: HomepageComponent },
+    { path: 'terms', component: TermsComponent }];
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-homepage',
   imports: [],
   templateUrl: './homepage.component.html',
