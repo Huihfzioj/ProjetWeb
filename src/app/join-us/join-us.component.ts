0eff978a0e5378a48a9b595345fe6474dba647cd
Mayssa1110 <168310485+Mayssa1110@users.noreply.github.com>
2025-04-09 22:15:27 +0100

    2eme test

--- src/app/join-us/join-us.component.ts
+++ src/app/join-us/join-us.component.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule for [(ngModel)]
 import { RouterLink } from '@angular/router'; // Import RouterLink for routerLink directive
 import { CommonModule } from '@angular/common'; // Import CommonModule for *ngIf, *ngFor, etc.
-=======
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
->>>>>>> 7a77b42e9a151f8072696ce34dda226c075a570b
 
 @Component({
   selector: 'app-join-us',
@@ -16,21 +10,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './join-us.component.html',
   styleUrls: ['./join-us.component.css']
 })
-<<<<<<< HEAD
 export class JoinUsComponent implements OnInit {
   isSignUpTab: boolean = true; // Initialize to show Sign Up tab by default
   userType: string = 'student'; // Default to student form
   showPassword: boolean = false;
 
   // Initialize form data objects
-=======
-export class JoinUsComponent {
-  isSignUpTab = true;
-  userType: 'student' | 'alumni' = 'student';
-  showPassword = false;
-
-  // Modèles de formulaire
->>>>>>> 7a77b42e9a151f8072696ce34dda226c075a570b
   student = {
     firstName: '',
     lastName: '',
@@ -52,7 +37,6 @@ export class JoinUsComponent {
     password: ''
   };
 
-<<<<<<< HEAD
   constructor() {}
 
   ngOnInit(): void {}
@@ -89,45 +73,4 @@ export class JoinUsComponent {
   onLogin(): void {
     console.log('Log In:', this.login);
   }
-=======
-  // Basculer entre Sign Up et Login
-  setActiveTab(isSignUp: boolean) {
-    this.isSignUpTab = isSignUp;
-    this.resetForms();
-  }
-
-  // Sélectionner le type d'utilisateur
-  selectUserType(type: 'student' | 'alumni') {
-    this.userType = type;
-  }
-
-  // Afficher/masquer le mot de passe
-  togglePasswordVisibility() {
-    this.showPassword = !this.showPassword;
-  }
-
-  // Soumission du formulaire Sign Up
-  onSubmit() {
-    if (this.userType === 'student') {
-      console.log('Inscription Étudiant:', this.student);
-      // TODO: Implémenter l'inscription
-    } else {
-      console.log('Inscription Alumni:', this.alumni);
-      // TODO: Implémenter l'inscription
-    }
-  }
-
-  // Soumission du formulaire Login
-  onLogin() {
-    console.log('Connexion:', this.login);
-    // TODO: Implémenter la connexion
-  }
-
-  // Réinitialiser les formulaires
-  private resetForms() {
-    this.student = { firstName: '', lastName: '', email: '', password: '' };
-    this.alumni = { fullName: '', graduationYear: '', email: '', id: '', department: '', password: '' };
-    this.login = { email: '', password: '', rememberMe: false };
-  }
->>>>>>> 7a77b42e9a151f8072696ce34dda226c075a570b
 }
